50c7b92174898afe6d5e042f4d15d51c91a306b9
siddhesh <sidpanhale@gmail.com>
2021-01-27 16:45:15 +0530

    first commit

---
+++ covid19-tracker/src/InfoBox.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
+
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox_cases ${!isRed && "infoBox_cases--green"}`}>
+          {cases}
+        </h2>
+
+        <Typography className="infoBox_total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
---
+++ covid19-tracker/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_nzt1GO46ANaubDrxiu-tX2lXJ0301OU",
+    authDomain: "covid19-tracker-75df9.firebaseapp.com",
+    projectId: "covid19-tracker-75df9",
+    storageBucket: "covid19-tracker-75df9.appspot.com",
+    messagingSenderId: "106352588223",
+    appId: "1:106352588223:web:df2243bb04615ddbc31d0a",
+    measurementId: "G-8CJCYN3RG6"
+  };
